0e57b65da0265a4c924a86dfa725253af535ef21
Grigoriy <gregory.zhiharev@gmail.com>
2015-04-05 22:46:14 +0400

    Updated TB. Now compile

--- testbench/testbench/topmodule.sv
+++ testbench/testbench/topmodule.sv
@@ -3,34 +3,32 @@
 
 `timescale 1ns/10ps
 module testbench ();
-        bit clk = 0;////////////////mine clock
+        bit clk = 0;
         
         
-//////////////
-////generetor clk
-/////////////////        
-        
+    //generetor clk   
+    initial begin
         forever #10 clk=~clk;
-///////////        
-/////incilisation interfaces
-//////                
-        wishbone instraction(clk);
-        wishbone data(clk);
-        control control(clk);
-//////////// 
-///////// interconnections for dut
-//////////////                
-        cpu_dut dut(
-                .CLK_I(control.clock),
-                .RES_I(control.reset),
-                .ADR_I(data.adr_in),
-                .ADR_O(data.adr_out),
-                .DAT_I(data.data_in),
-                .DAT_O(data.data_out)
-                      
-               ); 
+    end
+               
+    wishbone instraction(clk);
+    wishbone data(clk);
+    control control(clk);
+              
+    cpu dut(
+        .CLK_I(control.clk),
+        .RST_I(control.reset),
+        .ADR_I(data.adr_in),
+        .ADR_O(data.adr_out),
+        .DAT_I(data.data_in),
+        .DAT_O(data.data_out)         
+    ); 
 
-
+    initial begin
+        $display("START TEST");
+        #100;
+        $finish();
+    end
 
 endmodule
 
