8d8154b79aca3099da070ff140a383d9f603894d
AlexBolotnikov <blcorporation@mail.ru>
2015-04-12 23:13:03 +0300

    new enviroment

--- testbench/testbench/environment.sv
+++ testbench/testbench/environment.sv
@@ -1,32 +1,29 @@
 `ifdef ENVIRONMENT
 `define ENVIRONMENT
 
-//////////////new
-//////////////build
-//////////////reset
-//////////////cfg_dut
-//////////////start
-//////////////wait_for_end
-//////////////report
-//////////////run
+
 
 
 class environment
-///////////// how to connect c model???????
 
-        virtual  wishbone inst_cpu;
+
+        virtual wishbone inst_cpu;
         virtual wishbone data_cpu;
-        
+        virtual control cont_cpu;
+        driver dr;
+        scorebord sb;
+        monitor mon;        
         
         function new(virtual  wishbone inst_cpu, virtual wishbone data_cpu) begin
-                                
-        
+                      this wishbone = inst_cpu;
+                      this wishbone = data_cpu;           
+                      this control = cont_cpu;
         end
         
         
-      //////////////////build  
+    
         function bilt () begin 
-                $display("Enviroment: build is started",$time);
+                $display("Enviroment: build is started. %0t",$time);
                 
                 
                 
@@ -37,33 +34,62 @@ class environment
                 
                 
                 
-                $display("Enviroment: build is complited",$time);        
+                $display("Enviroment: build is complited. %0t",$time);        
         
         end
-//////////////////////reset
+
         function reset () begin
-                $display("Enviroment : reset is turned on")
-                $display("Enviroment : reset was be commited",$time);
+                $display("Enviroment : reset is turned on. %0t",$time);
+                cont_cpu.reset <= 1;
+                repeat(5) @(posedge cont_cpu clk);
+                inst_cpu.adr_out <=0;
+                inst_cpu.adr_in <= 0;
+                inst_cpu.data_in <=0;
+                inst_cpu.data_out <=0;
+                inst_cpu.we <= 0;
+                inst_cpu.act_in <=0;
+                insr_cpu.act_out <=0;
+                inst_cpu.sel_out <=0:
+                inst_cpu.sed_in <=0;
+                inst_cpu.cyc_out <=0;
+
+                data_cpu.adr_out <=0;
+                data_cpu.adr_in <= 0;
+                data_cpu.data_in <=0;
+                data_cpu.data_out <=0;
+                data_cpu.we <= 0;
+                data_cpu.act_in <=0;
+                data_cpu.act_out <=0;
+                data_cpu.sel_out <=0:
+                data_cpu.sed_in <=0;
+                data_cpu.cyc_out <=0;
+
+                $display("Enviroment : reset was be commited. %0t",$time);
                 
         
         end
-/////////////////// run
-        function run () begin
-                $display("Enviroment : run is on");
-                
-                $display("Enviroment : run is done");
+
+        function start () begin
+                $display("Enviroment : run is on. %0t", $time);
+                        fork 
+                                drv.start;
+                                mon.start;
+                                sb.start;
+                        join_any
+                        
+                $display("Enviroment : run is done. %0t", $time);
         
         
         end
-///////////////// wait for end
-        function wait_end () begin
-                $display("Enviroment : wait is on");
-                
-                $display("Enviroment : wait is done");
+
+        task wait_end () begin
+                $display("Enviroment : wait is on. %0t", $time);
+                repeat(10000) @(cont_cpu.clk);
+                $display("Enviroment : wait is done. %0t", $time);
         
         end
-//////////////// report
-        function report ()begin
+
+        function void report ()begin
                 $display()
         
         end
