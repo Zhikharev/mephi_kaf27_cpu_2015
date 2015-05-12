ba8ba577e58dd7079e422b18499f43fe0b6a8f93
Nataly B <gipnocow@rambler.ru>
2015-05-12 21:57:58 +0300

    File with Flags

---
+++ assembler/fullWithFlags.py
@@ -0,0 +1,15 @@
+def addFlags(ifile, ofile):
+    file = open(ifile, 'rb')
+    text = file.read()
+    file = open(ofile, 'wb')
+    count = 0
+    for char in text:
+        if count == 0:
+            file.write(bytes(chr(255), 'iso8859-1'))
+            count += 2
+
+            file.write(bytes(chr(char), 'iso8859-1'))
+            count -= 1
+
+    file.write(bytes(chr(0), 'iso8859-1'))
+    
