4187de5dd9aa445ff3bcb648d6d6fbcfc14e8a10
GerasimchukAleksey <gerasimchukaleksey@gmail.com>
2015-06-22 09:09:32 +0300

    CPU_TOP.v edited

--- rtl/cpu2/CPU_TOP.v
+++ rtl/cpu2/CPU_TOP.v
@@ -37,14 +37,14 @@ wire regwrite_wire;
 wire jump_wire;
 
 wire pcsrc_wire;
-wire [9:0] pc_in_wire;
+// wire [9:0] pc_in_wire;
 wire [9:0] pc_out_wire;
 
 // wire [31:0] srca_wire;
 wire [31:0] srcb_wire;
 wire [4:0] ra2_wire;
 wire [4:0] ra3_wire;
-wire [4:0] wd3_wire;
+wire [31:0] wd3_wire;
 wire we3_wire;
 wire [31:0] rd1_wire;
 wire [31:0] rd2_wire;
@@ -65,7 +65,7 @@ wire [9:0] instr_data_i_shift;
 PC PC(
 	.clk(sys_clk),
 	.reset(sys_rst),
-	.pc_in(pc_in_wire),//
+	.pc_in(pc_result2),//
 	.pcsrc(pcsrc_wire),//
 	.pc_out(pc_out_wire)//
 );
@@ -96,22 +96,38 @@ RF RF(
 	.ra1(ra1_wire),
 	.ra2(ra2_wire),
 	.ra3(ra3_wire),
-	.wd3(wd3_wire),
-	.we3(we3_wire),
+	.wd3(mem_result),
+	.we3(regwrite_wire),
 	.rd1(rd1_wire),
 	.rd2(rd2_wire)
 );
 
 //pc
-assign pcsrc_wire = branch_wire & zero_wire;//?
-assign mem_result = (memtoreg_wire == 1) ? data_data_i : aluresult_wire;
+assign pcsrc_wire = branch_wire & zero_wire;
+always @*
+	if memtoreg_wire & data_ack_i == 1
+		if instr_data_i[11] == 0
+			mem_result[15:0] = data_data_i;
+		else
+			mem_result[31:16] = data_data_i;
+	else
+		mem_result = aluresult_wire;
+
 assign pc_result = (pcsrc_wire == 1) ? pcbranch : pcplus4;
 assign signimm_shift = signimm << 1;
 assign pcbranch = signimm_shift + pcplus4;
 assign pcplus4 = pc_out_wire + 4;
 assign instr_data_i_shift = instr_data_i << 1;
-assign pc_result2 = (jump_wire == 1) ? instr_data_i_shift[7:0] : pcplus4[9:8];//?
+assign pc_result2 = (jump_wire == 1) ? {pcplus4[9:8], instr_data_i_shift[7:0]} : pc_result;
+
 //alu
-assign srcb_wire = (alusrc == 1) ? signimm : rd2_wire;//?
+assign srcb_wire = (alusrc == 1) ? signimm : rd2_wire;
+always @*
+	if instr_data_i[11] == 0
+		data_data_o[15:0] = rd2_wire;
+	else
+		data_data_o[31:0] = rd2_wire;
+assign data_addr_o = aluresult_wire;
+assign data_we_o = memwrite_wire;
 
 endmodule
