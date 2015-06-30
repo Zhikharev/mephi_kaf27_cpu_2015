81e2cd11e45f8581e1d7dfa0561158f4319f660d
GerasimchukAleksey <gerasimchukaleksey@gmail.com>
2015-06-30 04:08:42 +0300

    ALU.v edited

--- rtl/cpu2/ALU.v
+++ rtl/cpu2/ALU.v
@@ -13,6 +13,7 @@ module ALU(
     input [31:0] srca,
     input [31:0] srcb,
     input  [2:0] alucontrol,
+    input [3:0] shift,
     output [31:0] aluresult,
     output zero
     );
@@ -23,22 +24,50 @@ reg [31:0] aluresult_reg;
 always @*
 begin
     case (alucontrol)
-        3'b000 : aluresult_reg = srca + srcb;   // сложение
-        3'b001 : aluresult_reg = srca | srcb; // побитовое ИЛИ
-        3'b010 : aluresult_reg = srca & srcb; // побитовое И
-        3'b011 : aluresult_reg = srca ^ srcb; // побитовое ИСКЛ ИЛИ
-        3'b100 : aluresult_reg = ~(srca | srcb); // побитовое ИЛИ НЕ
-        3'b101 : aluresult_reg = srca << shift; //  логический сдвиг влево
-        3'b110 : aluresult_reg = (srca << shift) | (srca >> shift); // цикличекий сдвиг влево
+        3'b000 : 
+            begin
+                aluresult_reg = srca + srcb; // сложение (сложение с константой)
+                zero_reg = 1'b0;
+            end
+        3'b001 : 
+            begin
+                aluresult_reg = srca | srcb; // побитовое ИЛИ
+                zero_reg = 1'b0;
+            end
+        3'b010 : 
+            begin
+                aluresult_reg = srca & srcb; // побитовое И
+                zero_reg = 1'b0;
+            end
+        3'b011 : 
+            begin
+                aluresult_reg = srca ^ srcb; // побитовое ИСКЛ ИЛИ
+                zero_reg = 1'b0;
+            end
+        3'b100 : 
+            begin
+                aluresult_reg = ~(srca | srcb); // побитовое ИЛИ НЕ
+                zero_reg = 1'b0;
+            end
+        3'b101 : 
+            begin
+                aluresult_reg = srca << shift; // логический сдвиг влево
+                zero_reg = 1'b0;
+            end
+        3'b110 : 
+            begin
+                aluresult_reg = (srca << shift) | (srca >> shift); // цикличекий сдвиг влево
+                zero_reg = 1'b0;
+            end
         3'b111 : 
             begin
                 aluresult_reg = srca - srcb;
-                if (aluresult_reg == 0)
-                    zero_reg = 1;
+                if (aluresult_reg == 32'b0)
+                    zero_reg = 1'b1;
                 else
-                    zero_reg = 0;
+                    zero_reg = 1'b0;
             end
-        default : aluresult_reg = 0;        
+        default : aluresult_reg = 32'b0;        
     endcase  
 end
 
